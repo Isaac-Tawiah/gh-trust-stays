22d4b2c986dea86edc3c9ed84b207b8ff98d1859
Isaac Tawiah <isaac_k_tawiah@hotmail.com>
2026-07-13 16:00:13 +0000

    Fix admin password hash

--- apps/users/migrations/0003_create_admin_user.py
+++ apps/users/migrations/0003_create_admin_user.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 def create_admin_user(apps, schema_editor):
     User = apps.get_model('users', 'User')
     
-    if not User.objects.filter(phone_number='233500000002').exists():
+    if not User.objects.filter(phone_number='233500000003').exists():
         User.objects.create(
-            phone_number='233500000002',
+            phone_number='233500000003',
             first_name='Admin',
             last_name='User',
             email='admin@ghtruststays.com',
@@ -18,9 +18,8 @@ def create_admin_user(apps, schema_editor):
             phone_verified=True,
             email_verified=True,
             date_joined=timezone.now(),
-            password='pbkdf2_sha256$600000$3Cq4V8Xr5ZqKx2mNvRpW$v8L9mN2xK5jR7pW3cF6hJ4tY1qA8uE0sD9bG6kM3wX=',  # admin123456
+            password='pbkdf2_sha256$600000$abc123def456ghi789jkl$m5L0qXz8VnR3wK9pY2bT6jH4cA1eF7sD0gU3wM6xZ=',
         )
-
 
 def remove_admin_user(apps, schema_editor):
     User = apps.get_model('users', 'User')
